560a688df0438bbd35dd3ec3d6440aa12768be82
Tushar443 <moretushart33@gmail.com>
2020-08-04 17:10:51 +0530

    basic javascript

---
+++ JavaScript/hello.js
@@ -0,0 +1,91 @@
+// var a='Hello world';
+// console.log(a);
+
+// var arr=['tushar', 34 ,'More',45];
+// console.log(arr);
+
+
+// var variables =null+23;
+// console.log(variables);
+
+
+// var variables =undefined+23;
+// console.log(variables);
+
+
+// var variables ='23'+23;
+// console.log(variables);
+
+// var variables ='Tushar '+23;
+// var variables =undefined+23;
+// console.log(variables);
+// console.log(variables);
+
+// var variables =undefined+23;
+// console.log(variables);
+// var variables =undefined+23;
+// console.log(variables);
+
+// var variables =undefined+23;
+// console.log(variables);
+
+
+// class rectangular{
+//     constructor (y){
+//         this.y=y;
+//     }
+//     area(){
+//        return this.y*this.y;
+//     }
+// };
+
+// class square extends rectangular{
+//     constructor(z){
+//         super(z);
+//     }
+ 
+// };
+
+// // const x=new rectangular(3);
+// const p=new square(5);
+// console.log(p.area());
+
+
+/* Array Functions */
+var age=[23,45,67,87,89,54,33,4,55,67,23,45,2,1,34,56,6,76,50,45];
+
+//indexof
+//const x=age.indexOf(45,12);
+
+// for each
+// const x=age.forEach(function(ages){
+//     console.log(ages);
+// });
+
+// concat
+// var x = age.concat();
+//  console.log(x);
+
+//filter
+
+// const canDrink =age.filter(function(canDr){
+//     if (canDr>40 ){
+//         return true;
+//     }
+// });
+// console.log(canDrink);
+
+// in ES6 syntax above programe
+
+// const canDrink =age.filter(a=> a>=40);
+// console.log(canDrink);
+
+
+
+
+
+
+
+
+
+
